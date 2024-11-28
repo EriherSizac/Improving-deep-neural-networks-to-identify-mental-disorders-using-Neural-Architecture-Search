35fe3fc9c205d9f0f10f8be3b5d53814948ba3a8
erickAtPernexium <hernandezsilvaerick@gmail.com>
2024-11-28 14:13:24 -0600

    Update model_utils.py

--- D3T3C/D3TEC Dataset/ForVM/model_utils.py
+++ D3T3C/D3TEC Dataset/ForVM/model_utils.py
@@ -401,3 +401,63 @@ def build_tf_model_from_dict(model_dict, input_shape=(28, 28, 3)):
             model.add(DontCareLayer())
     
     return model
+
+
+
+def generate_layer(layer_type):
+    if layer_type == 'Conv2D':
+        return {
+            "type": "Conv2D",
+            "filters": random.randint(4, 16),  # Rango reducido para filtros
+            "strides": random.choice([1, 2]),
+            "activation": random.choice(["relu", "leaky_relu", "sigmoid", "tanh"])
+        }
+    elif layer_type == 'DepthwiseConv2D':
+        return {
+            "type": "DepthwiseConv2D",
+            "filters": random.randint(4, 16),  # Rango reducido para filtros
+            "strides": random.choice([1, 2]),
+            "activation": random.choice(["relu", "leaky_relu", "sigmoid", "tanh"])
+        }
+    elif layer_type == 'BatchNorm':
+        return {"type": "BatchNorm"}
+    elif layer_type == 'MaxPooling':
+        return {
+            "type": "MaxPooling",
+            "strides": random.choice([1, 2])
+        }
+    elif layer_type == 'Dropout':
+        return {
+            "type": "Dropout",
+            "rate": random.choice([0.2, 0.3, 0.4, 0.5])
+        }
+    elif layer_type == 'Dense':
+        return {
+            "type": "Dense",
+            "units": random.randint(1, 128),  # Rango reducido para unidades
+            "activation": random.choice(["relu", "leaky_relu", "sigmoid", "tanh"])
+        }
+    elif layer_type == 'Flatten':
+        return {"type": "Flatten"}
+    elif layer_type == 'DontCare':
+        return {"type": "DontCare"}
+    elif layer_type == 'Repetition':
+        return {
+            "type": "Repetition",
+            "repetition_layers": random.randint(1, 3),  # Limitar el número de capas para repetir
+            "repetition_count": random.randint(1, 2)  # Limitar el conteo de repeticiones
+        }
+    return {}
+
+
+def generate_random_architecture():
+    num_layers = 12
+    layers = []
+   
+    for _ in range(num_layers):
+        layer_type = random.choice([
+            'Conv2D', 'DepthwiseConv2D', 'BatchNorm', 'MaxPooling', 'Dropout', 
+            'Dense', 'Flatten', 'DontCare', 'Repetition'
+        ])
+        layers.append(generate_layer(layer_type))
+    return {"layers": layers}
