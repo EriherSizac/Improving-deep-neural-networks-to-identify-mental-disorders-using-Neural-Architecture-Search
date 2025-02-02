43378ee2ecf07841373bc916247a6f6e16b53248
erickAtPernexium <hernandezsilvaerick@gmail.com>
2025-02-02 13:32:14 -0600

    less batchsize

--- D3T3C/D3TEC Dataset/SurrogatesGenerator.py
+++ D3T3C/D3TEC Dataset/SurrogatesGenerator.py
@@ -1210,7 +1210,7 @@ def load_architectures_from_csv(csv_path):
     return architectures.tolist()
 
 train_models("EncodedChromosomes_V3.csv", "Dataset.csv", "./SM-27",
-             save_file="EncodedChromosomes_V3_results.csv", verbose=False)
+             save_file="EncodedChromosomes_V3_results.csv", verbose=False, batch_size=2)
 
 
 # %%
