37f7a4a32ea0b233c9e219b86bd7df722fb9956a
erickAtPernexium <hernandezsilvaerick@gmail.com>
2025-02-18 18:35:56 -0600

    Update SurrogatesGenerator.py

--- D3T3C/D3TEC Dataset/SurrogatesGenerator.py
+++ D3T3C/D3TEC Dataset/SurrogatesGenerator.py
@@ -1223,7 +1223,7 @@ def load_architectures_from_csv(csv_path):
     return architectures.tolist()
 
 train_models("EncodedChromosomes_V3.csv", "Dataset.csv", "./SM-27",
-             save_file="EncodedChromosomes_V3_results.csv", verbose=False, batch_size=100, epochs=25)
+             save_file="EncodedChromosomes_V3_results.csv", verbose=False, batch_size=64, epochs=25)
 
 
 # %%
