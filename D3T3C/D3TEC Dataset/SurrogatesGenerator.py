b003372ab5c965975fed2b28c90bf852eeab83f0
erihersizac <hernandezsilvaerick@gmail.com>
2025-02-19 13:25:44 -0600

    Update SurrogatesGenerator.py

--- D3T3C/D3TEC Dataset/SurrogatesGenerator.py
+++ D3T3C/D3TEC Dataset/SurrogatesGenerator.py
@@ -134,7 +134,7 @@ def decode_layer_params(encoded_params):
 
 class SelfAttention(nn.Module):
     def __init__(self, filters, attention_heads=4, activation=nn.ReLU(), verbose=False):
-        super(SelfAttentionLinear, self).__init__()
+        super(SelfAttention, self).__init__()
         # Aseguramos que filters sea divisible por attention_heads
         if filters % attention_heads != 0:
             if verbose:
